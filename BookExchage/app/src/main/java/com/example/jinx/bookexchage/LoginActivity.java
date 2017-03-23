388344f0f05c7acc661961615ca03851317d2268
gtxjinx <gtxmiracle@buaa.edu.cn>
2017-03-23 16:00:12 +0800

    1

---
+++ BookExchage/app/src/main/java/com/example/jinx/bookexchage/LoginActivity.java
@@ -0,0 +1,19 @@
+package com.example.jinx.bookexchage;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Jinx on 2017/3/23 15:10.
+ * 邮箱：gtxmiracle@buaa.edu.cn
+ */
+public class LoginActivity extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
